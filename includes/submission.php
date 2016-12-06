f75d5c138885c34611df07fb3c7bb827ff2f157f
Maksim Sholomov <mail@mshlmv.ru>
2016-12-06 13:46:57 +0500

    Submit to comment

--- includes/submission.php
+++ includes/submission.php
@@ -169,6 +169,30 @@ class WPCF7_Submission {
 			do_action( 'wpcf7_mail_failed', $contact_form );
 		}
 
+		//комментирование привязанной записи
+		if ($contact_form->wpcf7_to_quest == 1 && $contact_form->wpcf7_to_quest_id) {
+
+            $commentdata = [];
+
+            $to_quest_id = $contact_form->wpcf7_to_quest_id;
+            $name = $this->get_posted_data('your-name');
+            $email = $this->get_posted_data('your-email');
+            $message = $this->get_posted_data('your-message');
+            $answer_to_quest = $this->get_posted_data('answer_to_quest');
+
+            if ($answer_to_quest && $to_quest_id && $name && $email && $message) {
+                $commentdata['comment_author'] = $name;
+                $commentdata['comment_author_email'] = $email;
+                $commentdata['comment_author_url'] = $_SERVER['REMOTE_HOST'];
+                $commentdata['comment_author_IP'] = $_SERVER['REMOTE_ADDR'];
+                $commentdata['comment_post_ID'] = $to_quest_id;
+                $commentdata['comment_content'] = $message;
+                $commentdata['comment_approved'] = 0;
+
+                wp_insert_comment($commentdata);
+            }
+        }
+
 		$this->remove_uploaded_files();
 
 		return $this->status;
